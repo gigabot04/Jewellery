815adc6d436941aa4074f29bc38fb00fe128793a
Костя <GigaBOT4@yandex.ru>
2021-04-20 00:41:37 +0300

    слайдер

--- build/js/main.js
+++ build/js/main.js
@@ -52,3 +52,57 @@
     navheader.classList.toggle(`header__nav--active`);
   });
 }
+
+'use strict';
+{
+  // import Swiper from 'swiper/bundle';
+  new Swiper(`.shop__container`, {
+    navigation: {
+      nextEl: `.swiper-button-next`,
+      prevEl: `.swiper-button-prev`
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + '</span>';
+      },
+      renderFraction: function (currentClass, totalClass) {
+        return '<span class="' + currentClass + '"></span>' +
+                ' of ' +
+                '<span class="' + totalClass + '"></span>';
+      }
+    },
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+    loop: true,
+    a11y: {
+      enabled: true,
+    },
+    spaceBetween: 30,
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        pagination: {
+          type: 'fraction'
+        }
+      },
+      767: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        pagination: {
+          type: 'bullets'
+        }
+      },
+      1201: {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        pagination: {
+          type: 'bullets'
+        }
+      }
+    }
+  });
+}
+
