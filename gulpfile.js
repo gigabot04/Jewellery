02110712eda65083bbf72183dfe9adec98291590
Костя <GigaBOT4@yandex.ru>
2021-04-28 11:29:31 +0300

    Тест

--- gulpfile.js
+++ gulpfile.js
@@ -100,8 +100,7 @@ gulp.task(`copy`, function () {
   return gulp.src([
     `source/fonts/**/*.{woff,woff2}`,
     `source/img/**`,
-    `source/*.ico`,
-    'source/css/*.css'
+    `source/*.ico`
   ], {
     base: `source`
   })
